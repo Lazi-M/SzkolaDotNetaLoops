fa1d1d5c66537ee93bbf9be9923cfeeea86202e2
QoomWolf <43828977+QoomWolf@users.noreply.github.com>
2020-07-22 21:43:48 +0200

    Create ex5.cs
    zad. 5.
    
    Napisz program, który w konsoli wypisze tabliczkę mnożenia liczb od 1 do 10.

---
+++ ex5.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace SzkolaDotNetaLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} * {i} = {i*i}");
+            }
+
+        }
+    }
+}
+
+/*output
+0 * 0 = 0
+1 * 1 = 1
+2 * 2 = 4
+3 * 3 = 9
+4 * 4 = 16
+5 * 5 = 25
+6 * 6 = 36
+7 * 7 = 49
+8 * 8 = 64
+9 * 9 = 81
+10 * 10 = 100
+*/
