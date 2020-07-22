c934fa26f288c55d6e2ebe7a9d6acf7fd0b64797
QoomWolf <43828977+QoomWolf@users.noreply.github.com>
2020-07-22 21:15:19 +0200

    Update ex1.cs

--- ex1.cs
+++ ex1.cs
@@ -6,11 +6,11 @@ namespace SzkolaDotNetaLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How many numbers do you want to print");
+            Console.WriteLine("To which number should i print");
 
             int userInput = Int32.Parse(Console.ReadLine());
 
-            for (int i = 0; i < userInput; i++)
+            for (int i = 0; i <= userInput; i++)
             {
                 Console.WriteLine(i);
             }
